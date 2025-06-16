46f36b602687e0a803100035f261f8e5544e60df
黄明祁 <huang_mingqi.pfu@cn.fujitsu.com>
2025-06-16 16:43:56 +0800

    te

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>psh1 Hello {value} haha!!</div>;
+  return <div>psh1 Hello {value} haha- this is a dolphin test.</div>;
 }
 
 export default App;
